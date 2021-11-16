62a5620c9877babb8e34bd98d5749c200ba703b7
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-11-16 14:47:26 +0100

    installed sass & fontawesom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
 import './sass/main.scss';
 
 function App() {
   return (
     <>
-      <h1>testing</h1>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
     </>
   );
 }

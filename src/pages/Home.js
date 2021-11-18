63d37cd51ae2412a944620ea61e303f3818fd949
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-11-18 09:54:30 +0100

    added some styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,26 @@
+import CtaBtn from '../components/UI/CtaBtn';
+import heroImg from '../assets/images/man1.jpg';
+
+import './Home.scss';
+
 const Home = () => {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div className="hero">
+      <div className="container">
+        <div className="hero-wrapper">
+          <div className="hero-wrapper__cta">
+            <h1 className="hero-wrapper__heading-primary">
+              Men Collection 2021
+            </h1>
+            <h2 className="hero-wrapper__heading-secondary">New Arrivals</h2>
+            <CtaBtn>Shop Now</CtaBtn>
+          </div>
+          <div className="hero-img-wrapper">
+            <img src={heroImg} alt="man" />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 

bf67bdcc79fd9b66828480d6a183cfed110f1dcd
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-11-30 14:20:02 +0100

    forwarded props to swiper content

--- src/components/swiper/HeroSlide.js
+++ src/components/swiper/HeroSlide.js
@@ -1,22 +1,20 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import CtaBtn from '../UI/CtaBtn';
 import SwiperCore, { Autoplay } from 'swiper';
+import SwiperContent from './SwiperContent';
+
 import './HeroSlide.scss';
 
-// swiper bundle styles
 import 'swiper/swiper-bundle.min.css';
-
-// swiper core styles
 import 'swiper/swiper.min.css';
-
-// modules styles
 import 'swiper/components/navigation/navigation.min.css';
 import 'swiper/components/pagination/pagination.min.css';
 
 const HeroSlide = () => {
   SwiperCore.use([Autoplay]);
+
   return (
     <Swiper
+      onSlideChange={() => console.log('slide change')}
       autoplay={{
         delay: 5000,
         disableOnInteraction: false,
@@ -24,31 +22,22 @@ const HeroSlide = () => {
       className="hero-swiper"
     >
       <SwiperSlide>
-        <div className="slide-container">
-          <div className="cta-wrapper">
-            <h1 className="swiper-heading-primary">Men collection 2022</h1>
-            <h2 className="swiper-heading-secondary">New Arrivals</h2>
-            <CtaBtn>Shop Now</CtaBtn>
-          </div>
-        </div>
+        <SwiperContent
+          headingPrimary={'Women Collection 2022'}
+          headingSecondary={'New Season'}
+        />
       </SwiperSlide>
       <SwiperSlide>
-        <div className="slide-container">
-          <div className="cta-wrapper">
-            <h1 className="swiper-heading-primary">Women Collection 2022</h1>
-            <h2 className="swiper-heading-secondary">New Season</h2>
-            <CtaBtn>Shop Now</CtaBtn>
-          </div>
-        </div>
+        <SwiperContent
+          headingPrimary={'Men collection 2022'}
+          headingSecondary={'New Arrivals'}
+        />
       </SwiperSlide>
       <SwiperSlide>
-        <div className="slide-container">
-          <div className="cta-wrapper">
-            <h1 className="swiper-heading-primary">Men New Season</h1>
-            <h2 className="swiper-heading-secondary">Jackets</h2>
-            <CtaBtn>Shop Now</CtaBtn>
-          </div>
-        </div>
+        <SwiperContent
+          headingPrimary={'Men New Season 2022'}
+          headingSecondary={'Jackets'}
+        />
       </SwiperSlide>
     </Swiper>
   );

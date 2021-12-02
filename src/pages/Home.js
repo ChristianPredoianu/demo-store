8e4c4b0feae9381927d1e016f3e7eed55ae3d701
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-12-02 11:47:49 +0100

    work on categories

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,59 @@
+import useApi from '../hooks/useApi';
 import HeroSlide from '../components/swiper/HeroSlide';
+import CategoryCard from '../components/UI/CategoryCard';
 import Products from '../components/products/Products';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import electronicsImg from '../assets/images/electronics.jpg';
+import womensImg from '../assets/images/womens-clothing.jpg';
+import menImg from '../assets/images/mens-clothing.jpg';
+import jewleryImg from '../assets/images/jewlery.jpg';
 
 import './Home.scss';
 
 const Home = () => {
+  const { productsData, isLoadingData, error } = useApi(
+    'https://fakestoreapi.com/products'
+  );
+
+  const imgArray = [menImg, jewleryImg, electronicsImg, womensImg];
+  let categories;
+  let uniqueCategories;
+  let categoriesArray = [];
+
+  if (!isLoadingData) {
+    //New array with just the categories
+    categories = productsData.map((a) => a.category);
+    //Make an array with unique categories
+    uniqueCategories = [...new Set(categories)];
+    uniqueCategories.forEach((category) =>
+      categoriesArray.push({ category: category })
+    );
+    //Push images for every category since the given API does not include img for category
+    categoriesArray.forEach((category, index) => {
+      const img = imgArray[index];
+      category.categoryImg = img;
+    });
+  }
+
+  const categoryCards = categoriesArray.map((category, index) => (
+    <CategoryCard key={index} category={category} />
+  ));
+
   return (
-    <div className="hero">
-      <div className="container">
+    <div className="container">
+      <div className="hero">
         <HeroSlide />
-        <Products />
-        <h1>dsasdsad</h1>
       </div>
+      {isLoadingData && <LoadingSpinner />}
+      {!isLoadingData && (
+        <>
+          <section className="categories">{categoryCards}</section>
+          <section className="products">
+            <h3 className="products-heading">Products Overview</h3>
+            <Products products={productsData} />
+          </section>
+        </>
+      )}
     </div>
   );
 };

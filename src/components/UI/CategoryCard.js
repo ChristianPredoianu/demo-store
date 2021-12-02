72ea85a585ec81af3f4ee1621009098ef9019036
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-12-02 13:12:41 +0100

    uppercase firt letter in heading

--- src/components/UI/CategoryCard.js
+++ src/components/UI/CategoryCard.js
@@ -3,10 +3,21 @@ import Card from './Card';
 import './CategoryCard.scss';
 
 const CategoryCard = (props) => {
+  const cardHeading =
+    props.category.category.charAt(0).toUpperCase() +
+    props.category.category.slice(1);
+
   return (
     <Card>
-      <h5>{props.category.category}</h5>
-      <img src={props.category.categoryImg} alt="category" />
+      <div className="card-wrapper">
+        <h5 className="card-wrapper__heading">{cardHeading}</h5>
+        <h6 className="card-wrapper__subheading">Spring 2022</h6>
+        <img
+          src={props.category.categoryImg}
+          alt="category"
+          className="card-wrapper__img"
+        />
+      </div>
     </Card>
   );
 };

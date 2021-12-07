3ef552dede9c303f311059795c8fc63310c47d3f
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-12-07 15:16:47 +0100

    some js changes

--- src/components/UI/CategoryCard.js
+++ src/components/UI/CategoryCard.js
@@ -1,15 +1,22 @@
+import { useNavigate } from 'react-router-dom';
 import Card from './Card';
 
 import './CategoryCard.scss';
 
 const CategoryCard = (props) => {
+  const navigate = useNavigate();
+
   const cardHeading =
     props.category.category.charAt(0).toUpperCase() +
     props.category.category.slice(1);
 
+  const goToShopHandler = () => {
+    navigate('/shop', { state: props.category.category });
+  };
+
   return (
     <Card>
-      <div className="card-wrapper">
+      <div className="card-wrapper" onClick={goToShopHandler}>
         <h5 className="card-wrapper__heading">{cardHeading}</h5>
         <h6 className="card-wrapper__subheading">Spring 2022</h6>
         <img

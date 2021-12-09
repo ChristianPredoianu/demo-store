59dfc1c6763214e7254efba48cc344338b2be9ff
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-12-09 14:22:08 +0100

    work on filter

--- src/components/products/productsFilter/ProductsFilterList.js
+++ src/components/products/productsFilter/ProductsFilterList.js
@@ -1,16 +1,45 @@
 import './ProductsFilterList.scss';
 
 const ProductsFilterList = (props) => {
-  let categories;
-  console.log(props.categories);
+  console.log(props);
 
-  if (props.categories) {
-    categories = props.categories.map((category) => {
-      console.log(category);
-      return <li className="filter-list__item">{category}</li>;
-    });
-  }
-  return <ul className="filter-list">{categories}</ul>;
+  const test = () => {
+    console.log('dsadsa');
+  };
+  return (
+    <div className="filter-list">
+      <button
+        className="filter-list__item"
+        onClick={() => props.onClickedCategory('all')}
+      >
+        All
+      </button>
+      <button
+        className="filter-list__item"
+        onClick={() => props.onClickedCategory("men's clothing")}
+      >
+        Mens
+      </button>
+      <button
+        className="filter-list__item"
+        onClick={() => props.onClickedCategory("women's clothing")}
+      >
+        Womens
+      </button>
+      <button
+        className="filter-list__item"
+        onClick={() => props.onClickedCategory('jewlery')}
+      >
+        Jewlery
+      </button>
+      <button
+        className="filter-list__item"
+        onClick={() => props.onClickedCategory('electronics')}
+      >
+        Electronics
+      </button>
+    </div>
+  );
 };
 
 export default ProductsFilterList;

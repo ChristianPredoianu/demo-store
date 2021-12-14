2df5aee210e65a8aa8210cfaea537c3a101065a6
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-12-14 13:58:50 +0100

    added state for modal

--- src/components/products/ProductItem.js
+++ src/components/products/ProductItem.js
@@ -1,18 +1,36 @@
+import { useState } from 'react';
+import ProductsModal from '../UI/ProductsModal';
 import Card from '../UI/Card';
 
 import './ProductItem.scss';
 
 const ProductItem = (props) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModalHandler = () => setIsModalOpen(true),
+    closeModalHandler = () => setIsModalOpen(false);
+
   return (
-    <Card>
-      <div className="product-wrapper">
-        <img src={props.image} alt="product" className="product-img" />
-        <div className="product-info">
-          <h4>{props.title}</h4>
-          <h5>{props.price} $</h5>
+    <>
+      <Card>
+        <div className="product-wrapper" onClick={openModalHandler}>
+          <img src={props.image} alt="product" className="product-img" />
+          <div className="product-info">
+            <h4>{props.title}</h4>
+            <h5>{props.price} $</h5>
+          </div>
         </div>
-      </div>
-    </Card>
+      </Card>
+
+      {isModalOpen && (
+        <ProductsModal
+          onCloseModal={closeModalHandler}
+          productImg={props.image}
+          productTitle={props.title}
+          productPrice={props.price}
+        />
+      )}
+    </>
   );
 };
 

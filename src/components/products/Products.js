9414df9b3ffc6730dfd81728df34dbdcc87455f8
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-12-16 11:29:33 +0100

    added id to props

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -9,6 +9,7 @@ const Products = (props) => {
     output = props.products.map((product) => (
       <ProductItem
         key={product.id}
+        id={product.id}
         title={product.title}
         price={product.price}
         image={product.image}

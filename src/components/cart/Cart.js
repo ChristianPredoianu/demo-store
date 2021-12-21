5214af5b6bc3ae233a59af3f2b8c2607581556d2
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-12-21 14:01:35 +0100

    work on cart

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,13 +1,33 @@
+import { useState, useEffect, useContext } from 'react';
+
 import CartItem from './CartItem';
 import CtaBtn from '../UI/CtaBtn';
 
 import './Cart.scss';
+import CartContext from '../../store/cart-context';
 
 const Cart = () => {
+  const [cartItems, setCartItems] = useState(null);
+  let cartItem = null;
+  const cartCtx = useContext(CartContext);
+
+  console.log(cartCtx);
+
+  if (cartItems) {
+    cartItem = cartItems.map((cartItem) => (
+      <CartItem
+        key={cartItem.productId}
+        img={cartItem.productImg}
+        title={cartItem.productTitle}
+        price={cartItem.productPrice}
+      />
+    ));
+  }
+
   return (
     <div className="cart">
       <h3>Cart</h3>
-      <CartItem />
+      {cartItem}
       <p className="total-amount">Total Amount: </p>
       <CtaBtn>Checkout</CtaBtn>
     </div>

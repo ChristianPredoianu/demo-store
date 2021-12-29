88d6dfee3b1f9d9f24992210269cc8f9b7a92436
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-12-29 14:48:58 +0100

    imported module.scss and changed classnames

--- src/components/UI/Product-modal/Backdrop.js
+++ src/components/UI/Product-modal/Backdrop.js
@@ -1,7 +1,7 @@
-import './Backdrop.scss';
+import classes from './Backdrop.module.scss';
 
 const Backdrop = (props) => {
-  return <div className="backdrop" onClick={props.onClose} />;
+  return <div className={classes.backdrop} onClick={props.onClose} />;
 };
 
 export default Backdrop;

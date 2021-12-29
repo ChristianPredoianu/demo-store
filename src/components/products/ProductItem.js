d02b5d682fae0e00859bf38d2abc92488dce4742
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-12-29 12:48:35 +0100

    imported module.scss and changed classnames

--- src/components/products/ProductItem.js
+++ src/components/products/ProductItem.js
@@ -1,22 +1,26 @@
 import { useState } from 'react';
 import ProductModal from '../UI/Product-modal/ProductModal';
 import Card from '../UI/Card';
+
 import CartProvider from '../../store/CartProvider';
 
-import './ProductItem.scss';
+import classes from './ProductItem.module.scss';
 
 const ProductItem = (props) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModalHandler = () => setIsModalOpen(true),
     closeModalHandler = () => setIsModalOpen(false);
-  console.log('rendering');
 
   return (
     <>
       <Card>
-        <div className="product-wrapper" onClick={openModalHandler}>
-          <img src={props.image} alt="product" className="product-img" />
+        <div className={classes['product-wrapper']} onClick={openModalHandler}>
+          <img
+            src={props.image}
+            alt="product"
+            className={classes['product-img']}
+          />
           <div className="product-info">
             <h4>{props.title}</h4>
             <h5>{props.price} $</h5>

fae80ea56f6460ae77fac248f49d5e143bf19a53
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-12-29 12:40:08 +0100

    imported module.scss and changed classnames

--- src/components/nav/NavLinks.js
+++ src/components/nav/NavLinks.js
@@ -1,19 +1,31 @@
 import { Link } from 'react-router-dom';
-import './NavLinks.scss';
+import classes from './NavLinks.module.scss';
 
 const NavLinks = (props) => {
   return (
-    <ul className="nav-links">
-      <li className="nav-links__link" onClick={props.onCloseMobileMenu}>
+    <ul className={classes['nav-links']}>
+      <li
+        className={classes['nav-links__link']}
+        onClick={props.onCloseMobileMenu}
+      >
         <Link to="/"> Home</Link>
       </li>
-      <li className="nav-links__link" onClick={props.onCloseMobileMenu}>
+      <li
+        className={classes['nav-links__link']}
+        onClick={props.onCloseMobileMenu}
+      >
         <Link to="/Shop"> Shop</Link>
       </li>
-      <li className="nav-links__link" onClick={props.onCloseMobileMenu}>
+      <li
+        className={classes['nav-links__link']}
+        onClick={props.onCloseMobileMenu}
+      >
         <Link to="/About"> About</Link>
       </li>
-      <li className="nav-links__link" onClick={props.onCloseMobileMenu}>
+      <li
+        className={classes['nav-links__link']}
+        onClick={props.onCloseMobileMenu}
+      >
         <Link to="/Contact"> Contact</Link>
       </li>
     </ul>

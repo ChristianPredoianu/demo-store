a646a63969e8fd1a60cbdd30ed8bddd9c2e06d37
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-12-29 12:43:36 +0100

    imported module.scss and changed classnames

--- src/components/nav/Search.js
+++ src/components/nav/Search.js
@@ -1,7 +1,8 @@
 import { useRef } from 'react';
-import './Search.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+import classes from './Search.module.scss';
 
 const Search = (props) => {
   const searchInputEl = useRef(null);
@@ -11,24 +12,24 @@ const Search = (props) => {
   };
 
   return (
-    <div className="search">
+    <div className={classes.search}>
       <FontAwesomeIcon
         icon={faTimes}
-        className="search__close-icon"
+        className={classes['search__close-icon']}
         onClick={props.onClose}
       />
-      <div className="search-input">
+      <div className={classes['search-input']}>
         <label htmlFor="Search"></label>
         <input
           type="text"
           placeholder="Search"
           ref={searchInputEl}
-          className="search-input__input"
+          className={classes['search-input__input']}
         />
         <FontAwesomeIcon
           icon={faSearch}
           onClick={searchTermHandler}
-          className="search-input__search-icon"
+          className={classes['search-input__search-icon']}
         />
       </div>
     </div>

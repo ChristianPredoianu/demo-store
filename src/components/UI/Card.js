3fc56da26c900b624595de6180ce34a2f49453f6
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-12-29 14:38:21 +0100

    imported module.scss and changed classnames

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,7 +1,7 @@
-import './Card.scss';
+import classes from './Card.module.scss';
 
 const Card = (props) => {
-  return <div className="card">{props.children}</div>;
+  return <div className={classes.card}>{props.children}</div>;
 };
 
 export default Card;

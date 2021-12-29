0b53871e9089f90bcc422d18d10d4c6cd192ea7a
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-12-29 14:38:33 +0100

    imported module.scss and changed classnames

--- src/components/swiper/SwiperContent.js
+++ src/components/swiper/SwiperContent.js
@@ -1,13 +1,17 @@
 import CtaBtn from '../UI/CtaBtn';
 
-import './SwiperContent.scss';
+import classes from './SwiperContent.module.scss';
 
 const SwiperContent = (props) => {
   return (
-    <div className="slide-container slide-container--1">
-      <div className="cta-wrapper">
-        <h1 className="swiper-heading-primary">{props.headingPrimary}</h1>
-        <h2 className="swiper-heading-secondary">{props.headingSecondary}</h2>
+    <div className={classes['slide-container slide-container--1']}>
+      <div className={classes['cta-wrapper']}>
+        <h1 className={classes['swiper-heading-primary']}>
+          {props.headingPrimary}
+        </h1>
+        <h2 className={classes['swiper-heading-secondary']}>
+          {props.headingSecondary}
+        </h2>
         <CtaBtn>Shop Now</CtaBtn>
       </div>
     </div>

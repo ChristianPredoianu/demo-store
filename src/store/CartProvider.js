81c3b1e6d58777decf0e80cb9f1ffae7887a6580
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-01-04 14:42:07 +0100

    moved reducer to own file

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,65 +1,10 @@
 import { useReducer, useEffect } from 'react';
 import CartContext from './cart-context';
+import cartReducer from './reducers/cart-reducer';
 
 let defaultCartState = {
   items: [],
   totalAmount: 0,
-};
-
-const cartReducer = (state, action) => {
-  if (action.type === 'ADD') {
-    const updatedTotalAmount =
-      state.totalAmount + action.item.productPrice * action.item.amount;
-
-    const existingCartItemIndex = state.items.findIndex(
-      (item) => item.productId === action.item.productId
-    );
-    const existingCartItem = state.items[existingCartItemIndex];
-    let updatedItems;
-    console.log(existingCartItem);
-    if (existingCartItem) {
-      const updatedItem = {
-        ...existingCartItem,
-        amount: existingCartItem.amount + action.item.amount,
-      };
-      updatedItems = [...state.items];
-      updatedItems[existingCartItemIndex] = updatedItem;
-    } else {
-      updatedItems = state.items.concat(action.item);
-    }
-
-    return {
-      items: updatedItems,
-      totalAmount: updatedTotalAmount,
-    };
-  }
-
-  if (action.type === 'REMOVE') {
-    console.log('removing');
-    const existingCartItemIndex = state.items.findIndex(
-      (item) => item.productId === action.id
-    );
-    const existingItem = state.items[existingCartItemIndex];
-    console.log(existingItem);
-    const updatedTotalAmount = state.totalAmount - existingItem.productPrice;
-    let updatedItems;
-    if (existingItem.amount === 1) {
-      updatedItems = state.items.filter((item) => item.productId !== action.id);
-    } else {
-      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
-      updatedItems = [...state.items];
-      updatedItems[existingCartItemIndex] = updatedItem;
-    }
-
-    return {
-      items: updatedItems,
-      totalAmount: updatedTotalAmount,
-    };
-  }
-
-  if (action.type === 'UPDATE_STATE') {
-    return action.value;
-  }
 };
 
 const CartProvider = (props) => {

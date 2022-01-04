794b275b6c4d17317f2aebda0dde721e6787379e
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-01-04 14:33:20 +0100

    added functionallity for duplicate products on add & on remove

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -11,7 +11,45 @@ const cartReducer = (state, action) => {
     const updatedTotalAmount =
       state.totalAmount + action.item.productPrice * action.item.amount;
 
-    const updatedItems = state.items.concat(action.item);
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.productId === action.item.productId
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+    console.log(existingCartItem);
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
+  if (action.type === 'REMOVE') {
+    console.log('removing');
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.productId === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    console.log(existingItem);
+    const updatedTotalAmount = state.totalAmount - existingItem.productPrice;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.productId !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
 
     return {
       items: updatedItems,
@@ -45,8 +83,6 @@ const CartProvider = (props) => {
     if (cartState !== defaultCartState) {
       localStorage.setItem('products', JSON.stringify(cartState));
       //create and/or set a new localstorage variable called "state"
-
-      console.log(cartState);
     }
   }, [cartState]);
 

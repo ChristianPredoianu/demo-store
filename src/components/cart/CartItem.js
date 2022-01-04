58b3f2938d97061d122f02bf4cb9c6903b383f28
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-01-04 14:30:18 +0100

    added remove item

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -1,6 +1,8 @@
 import classes from './CartItem.module.scss';
 
 const CartItem = (props) => {
+  console.log(props.id);
+
   return (
     <div className={classes['cart-item']}>
       <img src={props.img} alt="" className={classes['product-img']} />
@@ -8,8 +10,13 @@ const CartItem = (props) => {
         <p className={classes['product-details__title']}>{props.title}</p>
         <p className={classes['product-details__price']}>{props.price}</p>
         <p className={classes['product-details__amount']}>{props.amount}</p>
+        <p
+          className={classes['product-details__remove-product']}
+          onClick={props.onRemoveFromCart}
+        >
+          Remove item
+        </p>
       </div>
-      <p className={classes['remove-product']}></p>
     </div>
   );
 };

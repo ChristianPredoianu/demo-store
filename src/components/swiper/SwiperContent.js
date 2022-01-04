02b469b320c82f388340296ada619c062023796c
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-01-04 14:31:38 +0100

    img change

--- src/components/swiper/SwiperContent.js
+++ src/components/swiper/SwiperContent.js
@@ -1,10 +1,11 @@
 import CtaBtn from '../UI/CtaBtn';
+import img1 from '../../assets/images/man1.jpg';
 
 import classes from './SwiperContent.module.scss';
 
 const SwiperContent = (props) => {
   return (
-    <div className={classes['slide-container slide-container--1']}>
+    <div className={classes['slide-container']}>
       <div className={classes['cta-wrapper']}>
         <h1 className={classes['swiper-heading-primary']}>
           {props.headingPrimary}
@@ -12,6 +13,7 @@ const SwiperContent = (props) => {
         <h2 className={classes['swiper-heading-secondary']}>
           {props.headingSecondary}
         </h2>
+        <img src={img1} alt="" />
         <CtaBtn>Shop Now</CtaBtn>
       </div>
     </div>

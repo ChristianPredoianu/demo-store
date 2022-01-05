83a9b7beddf0658a011d67e5be6b092b94dc79a4
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-01-05 16:04:34 +0100

    wrapped app in CartProvider

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CartProvider from './store/CartProvider';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </BrowserRouter>
     ,
   </React.StrictMode>,

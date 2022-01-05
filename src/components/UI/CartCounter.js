d3bb27fdc57839a5dbb4802f2377ff27028f12db
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-01-05 15:57:42 +0100

    added class

--- src/components/UI/CartCounter.js
+++ src/components/UI/CartCounter.js
@@ -5,7 +5,6 @@ import classes from './CartCounter.module.scss';
 const CartCounter = (props) => {
   return (
     <div className={classes['cart-counter-wrapper']}>
-      <label htmlFor="quantity">Quantity (between 1 and 5):</label>
       <div className={classes['cart-counter']}>
         <CartCounterBtn handleClick={props.onDecreaseCartCount}>
           -

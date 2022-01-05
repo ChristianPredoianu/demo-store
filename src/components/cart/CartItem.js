5dfcd55fb4d2f0419cd4f86aa53a4b609345797e
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-01-05 16:01:50 +0100

    added functionallity to remove and add when changing amount

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -1,15 +1,51 @@
+import { useState } from 'react';
+import CartCounter from '../UI/CartCounter';
+
 import classes from './CartItem.module.scss';
 
 const CartItem = (props) => {
-  console.log(props.id);
+  const [cartCounter, setCartCounter] = useState(0);
+
+  const decreaseCartCountHandler = () => {
+    if (cartCounter >= 1) {
+      setCartCounter((prevState) => prevState - 1);
+      props.onRemoveFromCart();
+    }
+  };
+
+  const increaseCartCountHandler = () => {
+    setCartCounter((prevState) => prevState + 1);
+    props.onAddToCart();
+  };
+
+  const changeCountHandler = (e) => {
+    if (e.target.value >= 1) {
+      setCartCounter(e.target.value);
+    }
+  };
 
   return (
     <div className={classes['cart-item']}>
-      <img src={props.img} alt="" className={classes['product-img']} />
+      <img src={props.productImg} alt="" className={classes['product-img']} />
       <div className={classes['product-details']}>
-        <p className={classes['product-details__title']}>{props.title}</p>
-        <p className={classes['product-details__price']}>{props.price}</p>
-        <p className={classes['product-details__amount']}>{props.amount}</p>
+        <p className={classes['product-details__title']}>
+          {props.productTitle}
+        </p>
+        <p className={classes['product-details__price']}>
+          {props.productPrice}
+        </p>
+        <p className={classes['product-details__amount']}>
+          {`Amount: ${props.productAmount}`}
+        </p>
+        <div className={classes['product-details__counter']}>
+          <CartCounter
+            onDecreaseCartCount={decreaseCartCountHandler}
+            onIncreaseCartCount={increaseCartCountHandler}
+            onChangeCount={changeCountHandler}
+            cartCount={cartCounter}
+          />
+        </div>
+
         <p
           className={classes['product-details__remove-product']}
           onClick={props.onRemoveFromCart}

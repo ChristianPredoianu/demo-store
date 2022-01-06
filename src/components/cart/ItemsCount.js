b792c88fd9b594f72548ba9fbf5a9e83774edc94
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-01-06 12:48:08 +0100

    removed some console.logs

--- src/components/cart/ItemsCount.js
+++ src/components/cart/ItemsCount.js
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import CartContext from '../../store/cart-context';
+
 import classes from './ItemsCount.module.scss';
 
 const ItemsCount = () => {
@@ -7,13 +8,9 @@ const ItemsCount = () => {
 
   const { items } = cartCtx;
 
-  console.log(items);
-
   const cartItemsCount = items.reduce((currNum, item) => {
     return currNum + item.amount;
   }, 0);
-
-  console.log(cartItemsCount);
 
   return <div className={classes.counter}>{cartItemsCount}</div>;
 };

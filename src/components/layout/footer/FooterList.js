367cbd8e5427702fb691ff3ceb0426ce431227b6
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-01-10 17:34:47 +0100

    added a class

--- src/components/layout/footer/FooterList.js
+++ src/components/layout/footer/FooterList.js
@@ -2,7 +2,7 @@ import classes from './FooterList.module.scss';
 
 const FooterList = (props) => {
   return (
-    <div>
+    <div className={classes['list-section']}>
       <h5 className={classes.heading}>{props.heading}</h5>
       <ul className={classes.list}>
         <li className={classes['list__item']}>{props.item1}</li>

dc436a325d17548430d76a22382bb20a13e2eb06
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-01-11 14:40:25 +0100

    imported ScrollToTop component

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './footer/Footer';
+import ScrollToTop from '../UI/ScrollToTop';
 
 const Layout = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <ScrollToTop />
       <Footer />
     </>
   );

d50814320032a2bac702a353f8b267a36d2bfc02
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-01-11 14:41:03 +0100

    added a classname

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -1,5 +1,4 @@
 import FooterList from './FooterList';
-import CtaBtn from '../../UI/CtaBtn';
 
 import footerData from '../../../footerData.json';
 
@@ -23,11 +22,7 @@ const Footer = () => {
 
   return (
     <footer className={classes.footer}>
-      <div className={classes.dsa}> {footerListItem}</div>
-
-      <div className={classes.cta}>
-        <CtaBtn>Subscribe</CtaBtn>
-      </div>
+      <div className={classes['footer-lists']}>{footerListItem}</div>
     </footer>
   );
 };

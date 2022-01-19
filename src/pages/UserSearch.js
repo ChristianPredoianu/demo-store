e372f9beec05207d68bc4abd3181f87dc1ff8120
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-01-19 15:19:30 +0100

    work on filter functionallity

--- src/pages/UserSearch.js
+++ src/pages/UserSearch.js
@@ -1,6 +1,7 @@
-import { useEffect } from 'react';
-import { useLocation, useSearchParams } from 'react-router-dom';
+import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import LoadingSpinner from '../components/UI/LoadingSpinner';
+import ProductsFilter from '../components/products/productsFilter/ProductsFilter';
 import Products from '../components/products/Products';
 
 import useApi from '../hooks/useApi';
@@ -8,32 +9,53 @@ import useApi from '../hooks/useApi';
 import classes from './UserSearch.module.scss';
 
 const UserSearch = () => {
-  let searchFilteredData;
   const { state } = useLocation();
-  const [searchTerm, setSearchTerm] = useSearchParams();
-  const userSearch = searchTerm.get('q');
+  const [filterCategory, setFilterCategory] = useState(null);
+  const [isUserSearch, setIsUserSearch] = useState(true);
 
   const { productsData, isLoadingData, error } = useApi(
     'https://fakestoreapi.com/products'
   );
 
-  if (productsData && userSearch) {
-    searchFilteredData = productsData.filter((product) =>
-      product.title.toLowerCase().includes(userSearch)
+  let searchFilteredProducts, filteredProducts;
+
+  if (!isLoadingData) {
+    searchFilteredProducts = productsData.filter((product) =>
+      product.title.toLowerCase().includes(state)
     );
-    console.log(searchFilteredData);
+    console.log(searchFilteredProducts);
   }
 
-  useEffect(() => {
-    if (state !== null) {
-      setSearchTerm({ q: state });
-    }
-  }, [state, setSearchTerm]);
+  const filteredProductsHandler = (selectedCategory) => {
+    setFilterCategory(selectedCategory);
+    setIsUserSearch(false);
+  };
+
+  if (!isLoadingData) {
+    filteredProducts = productsData.filter(
+      (product) => product.category === filterCategory
+    );
+
+    /// ------ADD THIS------------
+    /*   filteredProducts === 'all'
+    ? (userFilteredProducts = productsData)
+    : (userFilteredProducts = productsData.filter(
+        (product) => product.category === filteredProducts
+      )); */
+  }
 
   return (
     <div className="container">
-      {searchFilteredData && <Products products={searchFilteredData} />}
-      {!searchFilteredData && <LoadingSpinner />}
+      {isLoadingData && <LoadingSpinner />}
+      <ProductsFilter
+        productsData={productsData}
+        onFilteredProducts={filteredProductsHandler}
+      />
+      {searchFilteredProducts && !filterCategory && (
+        <Products products={searchFilteredProducts} />
+      )}
+
+      {!isUserSearch && <Products products={filteredProducts} />}
     </div>
   );
 };

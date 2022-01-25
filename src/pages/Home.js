2abf77faf38ef8fa9711afa7de36e501254f72bb
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-01-25 17:18:42 +0100

    work on scrolltrigger animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
-import useApi from '../hooks/useApi';
+import { useEffect, useState, useMemo } from 'react';
+
 import HeroSlider from '../components/swiper/HeroSlider';
 import CategoryCard from '../components/UI/CategoryCard';
 import Products from '../components/products/Products';
@@ -8,35 +9,57 @@ import womensImg from '../assets/images/womens-clothing.jpg';
 import menImg from '../assets/images/mens-clothing.jpg';
 import jewleryImg from '../assets/images/jewlery.jpg';
 
+import useApi from '../hooks/useApi';
+import useGsapScrollReveal from '../hooks/useGsapScrollReveal';
+
 import classes from './Home.module.scss';
 
 const Home = () => {
+  const [categories, setCategories] = useState([]);
   const { productsData, isLoadingData, error } = useApi(
     'https://fakestoreapi.com/products'
   );
+  const { addToRefs, scrollRevealAnimation } = useGsapScrollReveal();
 
-  const imgArray = [menImg, jewleryImg, electronicsImg, womensImg];
-  let categoriesArray = [];
+  const imgArray = useMemo(
+    () => [menImg, jewleryImg, electronicsImg, womensImg],
+    []
+  );
 
-  if (!isLoadingData) {
-    let categories, uniqueCategories;
-    //New array with just the categories
-    categories = productsData.map((a) => a.category);
-    //Make an array with unique categories
-    uniqueCategories = [...new Set(categories)];
-    uniqueCategories.forEach((category) =>
-      categoriesArray.push({ category: category })
-    );
-    //Push images for every category since the given API does not include img for category
-    categoriesArray.forEach((category, index) => {
-      const img = imgArray[index];
-      category.categoryImg = img;
-    });
+  let categoryCard;
+
+  if (categories) {
+    categoryCard = categories.map((category, index) => (
+      <div key={index} ref={addToRefs}>
+        <CategoryCard category={category} />
+      </div>
+    ));
   }
 
-  const categoryCard = categoriesArray.map((category, index) => (
-    <CategoryCard key={index} category={category} />
-  ));
+  useEffect(() => {
+    if (!isLoadingData) {
+      let categories,
+        uniqueCategories,
+        categoriesArray = [];
+      //New array with just the categories
+      categories = productsData.map((a) => a.category);
+      //Make an array with unique categories
+      uniqueCategories = [...new Set(categories)];
+      uniqueCategories.forEach((category) => {
+        categoriesArray.push({ category: category });
+      });
+      //Push images for every category since the given API does not include img for category
+      categoriesArray.forEach((category, index) => {
+        const img = imgArray[index];
+        category.categoryImg = img;
+      });
+      setCategories(categoriesArray);
+    }
+  }, [isLoadingData, productsData, imgArray]);
+
+  useEffect(() => {
+    if (categories) scrollRevealAnimation();
+  }, [categories, scrollRevealAnimation]);
 
   return (
     <>
@@ -46,7 +69,7 @@ const Home = () => {
         </section>
         {isLoadingData && <LoadingSpinner />}
         {!isLoadingData && (
-          <section className={classes.categories}>{categoryCard}</section>
+          <section className={classes.categories}>{categoryCard} </section>
         )}
         <section>
           <h3 className={classes['products-heading']}>Products Overview</h3>

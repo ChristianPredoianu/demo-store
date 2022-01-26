408dcb64ca3e304b44bf1f7a0c55edfab8311857
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-01-26 15:01:52 +0100

    footer to only show is data is loaded

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -3,13 +3,17 @@ import Header from './Header';
 import Footer from './footer/Footer';
 import ScrollToTop from '../UI/ScrollToTop';
 
+import useApi from '../../hooks/useApi';
+
 const Layout = () => {
+  const { isLoadingData } = useApi('https://fakestoreapi.com/products');
+
   return (
     <>
       <Header />
       <Outlet />
       <ScrollToTop />
-      <Footer />
+      {!isLoadingData && <Footer />}
     </>
   );
 };

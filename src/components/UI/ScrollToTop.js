02d643a80feacc721007a2a6308306c3f0052d6a
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-02-02 14:05:11 +0100

    rearranged import statements

--- src/components/UI/ScrollToTop.js
+++ src/components/UI/ScrollToTop.js
@@ -1,9 +1,7 @@
 import { useState, useEffect } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
-
 import classes from './ScrollToTop.module.scss';
 
 const ScrollToTop = () => {

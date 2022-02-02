cd3e91a72b9fbb90150c553d51fad39506eba524
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-02-02 13:44:32 +0100

    removed isLoadingData to only render footer when data is loaded

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -3,17 +3,13 @@ import Header from './Header';
 import Footer from './footer/Footer';
 import ScrollToTop from '../UI/ScrollToTop';
 
-import useApi from '../../hooks/useApi';
-
 const Layout = () => {
-  const { isLoadingData } = useApi('https://fakestoreapi.com/products');
-
   return (
     <>
       <Header />
       <Outlet />
       <ScrollToTop />
-      {!isLoadingData && <Footer />}
+      <Footer />
     </>
   );
 };

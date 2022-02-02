24d372ccf531450efbd6abf62586529e8bcc91bd
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-02-02 14:02:28 +0100

    removed whitespace

--- src/components/UI/CategoryCard.js
+++ src/components/UI/CategoryCard.js
@@ -19,7 +19,6 @@ const CategoryCard = (props) => {
       <div className={classes['card-wrapper']} onClick={goToShopHandler}>
         <h5 className={classes['card-wrapper__heading']}>{cardHeading}</h5>
         <h6 className={classes['card-wrapper__subheading']}>Spring 2022</h6>
-
         <img src={props.category.categoryImg} alt="category" />
       </div>
     </Card>
